117f51845d2a1c6a02f28896681c02094c3f13e9
DSM3 <fatec-dsm3@academico.intra>
2025-02-10 20:20:41 -0300

    Dia 1

---
+++ TP2/10/02/aula01.py
@@ -0,0 +1,8 @@
+valor = float(input("Digite o valor da compra: "))
+valortotal = valor * 10
+print(f"O valor total é {valortotal}")
+
+# O f antes das aspas, serve para concatenar a variável e mostrá-la na tela normalmente.
+
+#Arredondamento de valores
+print(f"área = {area:.2f} cm2")
