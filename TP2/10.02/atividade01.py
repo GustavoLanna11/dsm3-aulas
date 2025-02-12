7835bce4df5388eb696de4832aae157c61bb6aad
DSM3 <fatec-dsm3@academico.intra>
2025-02-12 20:42:26 -0300

    dsm3

--- TP2/10.02/atividade01.py
+++ TP2/10.02/atividade01.py
@@ -1,5 +1,17 @@
+#Exercício 1
+print("Olá, vendedor! Seu salário fixo é: R$ 1500, mais R$50 de bônus por software vendido!")
+qtdSoftwareVendido = int(input("Digite a quantidade de softwares vendidos: "))
+bonusSoftware = qtdSoftwareVendido * 50
+salario = 1500 + bonusSoftware
+print(f"Seu salário final é de: {salario}")
 
+#Exercício 2
+valorPrestacao = float(input("Digite o valor da prestação: "))
+taxaJuros = int(input("Digite a taxa de juros imposta pelo banco: "))
+tempo = int(input("Digite a quantidade de meses em atraso: "))
 
+valorAtraso = valorPrestacao + (valorPrestacao*(taxaJuros/100)*100)
+print(f"O valor da prestação em atraso é: {valorAtraso}")
 
 #Exercício 3
 temperaturaC = int(input("Digite a temperatura em Celsius: "))
@@ -15,3 +27,5 @@ anoAtual = datetime.now().year
 idade = anoAtual - anoNasc
 print(f"O nome da pessoa é: {nome}")
 print(f"A idade da pessoa é: {idade}")
+
+#Exercício 5
