f89b342f0b2d44b189c10bb30fc3e63f9d12fca6
DSM3 <fatec-dsm3@academico.intra>
2025-02-17 21:50:52 -0300

    dsm3

--- TP2/17.02/aula02.py
+++ TP2/17.02/aula02.py
@@ -7,3 +7,18 @@ if media > 6:
 else:
     print("Reprovado")
 
+#Estrutura condicional - Case
+opcao = int(input("1 - sacar | 2 - extrato | 3 - sair | Escolha uma opção"))
+match opcao:
+    case 1:
+        print("Escolheu a opção sacar")
+        valor = float(input("Digite o valor do saque: R$:"))
+        print(f"Sacando da sua conta o valor de {valor}...")
+    case 2:
+        print("Escolheu a opção extrato")
+        dias = int(input("Digite a quantidade de dias do extrato R$: "))
+        print(f"Retirando o extrato de {dias} dias...")
+    case 3: 
+        exit
+    case _:
+        print("Opção inválida")
