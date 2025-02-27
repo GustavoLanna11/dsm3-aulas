bb4caf6479dba32aad7beb368f4d82963bd93508
DSM3 <fatec-dsm3@academico.intra>
2025-02-26 21:26:02 -0300

    dsm3

--- TP2/24.02/atividade_02.py
+++ TP2/24.02/atividade_02.py
@@ -47,4 +47,38 @@ match simbolo:
         print("Opção inválida")
 
 #Exercício 4
-num1 = int(input(""))
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+if num1 > num2:
+    subtracao = num1 - num2
+elif num2 > num1:
+    subtracao = num2 - num1
+elif num1 == num2:
+    print("Números iguais! ")
+
+#Exercício 5
+nome1 = str(input("Digite o nome da primeira pessoa: "))
+altura1 = float(input("Digite a altura da primeira pessoa: "))
+nome2 = str(input("Digite o nome da segunda pessoa: "))
+altura2 = float(input("Digite a altura da segunda pessoa: "))
+
+if altura1 > altura2:
+    print(f"A pessoa mais alta é {nome1} com {altura1} cm de altura")
+elif altura2 > altura1:
+    print(f"A pessoa mais alta é {nome2} com {altura2} cm de altura")
+elif altura1 == altura2:
+    print(f"{nome1} e {nome2} possuem a mesma altura: {altura1}")
+
+#Exercício 6
+valorProd = float(input("Digite o valor de compra do produto: "))
+if valorProd <= 20.00:
+    print("Você teve 45% de lucro!")
+    lucro = valorProd * 0.45
+    print(f"Valor de venda: {lucro}")
+elif valorProd > 20.00:
+    print("Você teve 30% de lucro!")
+    lucro = valorProd * 0.30
+    print(f"Valor de venda: {lucro}")
+
+#Exercício 7
+nome = str(input("Digite o "))
