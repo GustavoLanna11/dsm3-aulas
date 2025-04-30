f061dd84cc776e85bccd870a3d87259d5f7e23d5
DSM3 <fatec-dsm3@academico.intra>
2025-04-29 21:51:48 -0300

    dsm3

--- DW3/the-games-front-end-react-e-api/front-end-react/.next/static/chunks/webpack.js
+++ DW3/the-games-front-end-react-e-api/front-end-react/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab1039fcbf70b4dc"; }
+/******/ 		__webpack_require__.h = function() { return "cafed8ef57b6a37f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- DW3/the-games-front-end-react-e-api/front-end-react/.next/static/webpack/webpack.ba892ff0ccf9d046.hot-update.js
+++ DW3/the-games-front-end-react-e-api/front-end-react/.next/static/webpack/webpack.ba892ff0ccf9d046.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7fe5eb31532dbb60"; }
+/******/ 	__webpack_require__.h = function() { return "42c7816956281040"; }
 /******/ }();
 /******/ 
 /******/ }
